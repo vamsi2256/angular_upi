91dc8bef863478cae9c4fc19d9f12b21c579225b
vamsi <brugumallavamsi@gmail.com>
2021-01-13 18:12:18 +0530

    register comp executed sucessfully

--- app/register/register.js
+++ app/register/register.js
@@ -2,12 +2,30 @@ var app=angular.module('upiApp',[]);
 
 app.component('registerComponent',{
     templateUrl:"register/register.html",
-    controller:[function registerController(){
+    controller:function registerController($http){
         var self=this;
         self.name=""
         self.email=""
         self.city=""
         self.mobile_number=""
         self.pwd=""
-    }]
+        self.register=()=>{
+            $http.post("http://localhost:3001/Register",{
+                name:self.name,
+                email:self.email,
+                city:self.city,
+                mobileNumber:self.mobile_number,
+                password:self.pwd
+            }).then((response)=>{
+                if (response.data==='data Inserted'){
+                     self.msg = "Data Submitted Successfully!";
+                    //  $loction.path("")
+                     console.log("sucess")}
+                else{
+                    self.msg = "Service not Exists";
+                    console.log("failure")
+                    }
+                });
+        }
+    }
 })
